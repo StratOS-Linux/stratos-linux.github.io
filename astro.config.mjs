bdd545c139c5c8befb608bdbb19bf3ce79bd7b85
zstg <o0vckutt@duck.com>
2024-04-20 13:01:50 +0530

    Title change

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     integrations: [
 	starlight({
 	    plugins: [starlightImageZoom()],
-	    title: 'StratOS Documentation',
+	    title: 'StratOS Linux',
 	    customCss: process.env.NO_GRADIENTS ? [	'./src/styles/_global.css'] : ['./src/styles/landing.css', 	'./src/styles/_global.css'],
 	    // customCss: [
 	    // 	       './src/styles/custom.css',
